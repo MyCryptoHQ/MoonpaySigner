610e738045368834cf57a158d6e5ed541b49419a
blurpesec <hahn.michael.f@gmail.com>
2020-03-26 13:15:42 -0700

    remove endpoint, to use base endpoint instead

--- terraform/modules/api-gateway-lambda/lambda.tf
+++ terraform/modules/api-gateway-lambda/lambda.tf
@@ -46,24 +46,21 @@ resource "aws_lambda_permission" "lambda_permission" {
   source_arn    = "${aws_api_gateway_rest_api.moonpay_api_gateway.execution_arn}/*/*"
 
   depends_on = [
-    aws_api_gateway_rest_api.moonpay_api_gateway,
-    aws_api_gateway_resource.proxy,
+    aws_api_gateway_rest_api.moonpay_api_gateway
   ]
 }
 
 resource "aws_api_gateway_rest_api" "moonpay_api_gateway" {
   name = "MoonpaySigner"
-}
-
-resource "aws_api_gateway_resource" "proxy" {
-  rest_api_id = aws_api_gateway_rest_api.moonpay_api_gateway.id
-  parent_id   = aws_api_gateway_rest_api.moonpay_api_gateway.root_resource_id
-  path_part   = "moonpay"
+	description = "The signer REST API"
+  endpoint_configuration {
+    types = ["EDGE"]
+  }
 }
 
 resource "aws_api_gateway_method" "request_method" {
   rest_api_id   = aws_api_gateway_rest_api.moonpay_api_gateway.id
-  resource_id   = aws_api_gateway_resource.proxy.id
+  resource_id   = aws_api_gateway_rest_api.moonpay_api_gateway.root_resource_id
   http_method   = "POST"
   authorization = "NONE"
 }
