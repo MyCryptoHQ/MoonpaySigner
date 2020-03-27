8ac2405bb2fe70979431bbfa7d99d397e31dc5eb
blurpesec <hahn.michael.f@gmail.com>
2020-03-27 10:07:20 -0700

    changed endpoint to /sign

--- terraform/modules/api-gateway-lambda/cors.tf
+++ terraform/modules/api-gateway-lambda/cors.tf
@@ -1,5 +1,5 @@
 resource "aws_api_gateway_resource" "cors_resource" {
-  path_part   = "testtest"
+  path_part   = "sign"
   parent_id   = aws_api_gateway_rest_api.moonpay_api_gateway.root_resource_id
   rest_api_id = aws_api_gateway_rest_api.moonpay_api_gateway.id
 }
