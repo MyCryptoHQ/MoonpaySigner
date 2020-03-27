2d690d175d0f2b2d0d9838fd5aeeb22c02821e9a
blurpesec <hahn.michael.f@gmail.com>
2020-03-27 14:41:29 -0700

    added readme with directions to deploy

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,7 @@
 module "api-gateway-lambda" {
-  environment = "prod"
-  source      = "./modules/api-gateway-lambda"
-  filename    = "../moonpaysigner.zip"
-  endpoint    = var.endpoint
+  environment        = "prod"
+  source             = "./modules/api-gateway-lambda"
+  filename           = "../moonpaysigner.zip"
+  endpoint           = var.endpoint
+  moonpay_secret_key = var.moonpay_secret_key
 }
